a9cde3eeca78d5e312583785f12002013cd5d114
shashank <shashank.shekhardubey@gmail.com>
2020-06-29 06:11:37 +0200

    added replica refresh

--- benchmark/counter/counter_mix_rw.ml
+++ benchmark/counter/counter_mix_rw.ml
@@ -3,15 +3,15 @@ open Lwt.Infix
 module Counter: Irmin.Contents.S with type t = int64 = struct
 	type t = int64
 	let t = Irmin.Type.int64
-	let mc = ref 0
+	let mc = ref 0 
     let merge ~old a b = 
-        (* Printf.printf "\n** merging %d %d" (Int64.to_int a) (Int64.to_int b); *)
-        mc:=!mc+1;
-	    let open Irmin.Merge.Infix in
+      mc:=!mc + 1 ;
+    let open Irmin.Merge.Infix in
 		old () >|=* fun old ->
         let old = match old with None -> 0L | Some o -> o in
         let (+) = Int64.add and (-) = Int64.sub in 
         (* Printf.printf "  conflict:  %d  a=%d  b=%d  old=%d" !mc (Int64.to_int a) (Int64.to_int b) (Int64.to_int old); *)
+        Printf.printf "  conflict:  %d  " !mc;
         a + b - old
         
         let merge = Irmin.Merge.(option (v t merge))
@@ -19,69 +19,50 @@ end
 
 module Scylla_kvStore = Irmin_scylla.KV(Counter)
 
-let print msg client= () (*Printf.printf "\n** %s : %s" msg client*)
-
 let updateMeta meta_name msg time = 
     let _, time_sum, cnt = !meta_name in
     let count = cnt + 1 in  
     let t = time_sum +. time in
-    (* Printf.printf "\n** meta %s  %f  %d" msg t count; *)
     meta_name := (msg, t, count)
 
 let mergeBranches outBranch currentBranch opr_meta = 
     let stime = Unix.gettimeofday () in 
         ignore @@ Scylla_kvStore.merge_into ~info:(fun () -> Irmin.Info.empty) outBranch ~into:currentBranch;
     let etime = Unix.gettimeofday () in
-    Printf.printf "  *%f*" (etime -. stime);
-        updateMeta opr_meta "mergebranches" (etime -. stime);
+      updateMeta opr_meta "mergebranches" (etime -. stime);
 
         Lwt.return_unit
 
 let rec mergeOpr branchList currentBranch currentBranch_string repo opr_meta = 
-    (* Printf.printf "\nthis is %s in mergeOpr" currentBranch_string; *)
-    (match branchList with 
+    match branchList with 
     | h::t -> 
-            (* Printf.printf "\ncurrent branch to merge: '%s'  currentbranch_string: '%s'" h currentBranch_string; *)
             if (currentBranch_string <> h) then (  
-                Printf.printf "\n** merge of %s into %s" h currentBranch_string;
-                (* Scylla_kvStore.of_branch repo h >>= fun branch ->  *)
+              (* Printf.printf "\n** merge of %s into %s" h currentBranch_string; *)
                 Scylla_kvStore.Branch.get repo h >>= fun other_head_cmt ->
                 Scylla_kvStore.of_commit other_head_cmt >>= fun other_head ->
-                (* Scylla_kvStore.Branch.get repo currentBranch_string >>= fun current_head_cmt ->
-                Scylla_kvStore.of_commit current_head_cmt >>= fun current_head -> *)
-
-                    (* ignore @@ mergeBranches branch currentBranch opr_meta; *)
+                
                     ignore @@ mergeBranches other_head currentBranch opr_meta;
-                    (* ignore @@ mergeBranches current_head currentBranch opr_meta; *)
-
+                    
                     mergeOpr t currentBranch currentBranch_string repo opr_meta
                     )
                     else
                     mergeOpr t currentBranch currentBranch_string repo opr_meta
-    | _ -> (*print_string "branch list empty";*) 
-    (* mergeBranches currentBranch currentBranch opr_meta fix this *)
-        Lwt.return_unit);
+    | _ -> Lwt.return_unit
         
-        Lwt.return_unit
-
 let mergeOpr_help branchList currentBranch currentBranch_string repo opr_meta =
 
-  Scylla_kvStore.Branch.get repo currentBranch_string >>= fun current_head_cmt ->
-  Scylla_kvStore.of_commit current_head_cmt >>= fun current_head ->
+  (* Scylla_kvStore.Branch.get repo currentBranch_string >>= fun current_head_cmt ->
+  Scylla_kvStore.of_commit current_head_cmt >>= fun current_head -> *)
 
-  mergeOpr branchList current_head currentBranch_string repo opr_meta;
-
-  mergeBranches current_head currentBranch opr_meta
+  mergeOpr branchList currentBranch currentBranch_string repo opr_meta
+  (* ;mergeBranches current_head currentBranch opr_meta *)
 
 let createValue () =
     Int64.of_int (Random.int 10)
-    (* Int64.of_int 1 *)
 
 let getvalue private_branch_anchor lib client get_meta =
-    print "getvalue" client;
     let stime = Unix.gettimeofday() in
-        Scylla_kvStore.get private_branch_anchor [lib] >>= fun item -> 
-        (* Printf.printf "\nclient= %s  key=%s  value=%d" client lib (Int64.to_int item);  *)
+        Scylla_kvStore.get private_branch_anchor [lib] >>= fun item -> (*Printf.printf "item = '%d'" (Int64.to_int item);*)
         ignore item;
     let etime = Unix.gettimeofday() in
     updateMeta get_meta "getvalue" (etime -. stime);
@@ -91,13 +72,12 @@ let getvalue private_branch_anchor lib client get_meta =
 let rec build liblist private_branch_anchor repo client set_meta get_meta rw = (*cbranch_string as in current branch is only used for putting string in db*)
     match liblist with 
     | lib :: libls -> 
-        (* find_in_db lib private_branch_anchor >>= fun boolval ->  *)
              
         (match rw with 
-            | "post_write" -> (print "post_write build" client;
+            | "post_write" -> ( 
                         let v = createValue () in
                         let stime = Unix.gettimeofday() in
-                        
+                        (* Printf.printf "\n-----------------------\nsetting: client: %s, key: %s, value = %d" client lib (Int64.to_int v); *)
                         ignore @@ Scylla_kvStore.set_exn ~info:(fun () -> Irmin.Info.empty) 
                                                 private_branch_anchor [lib] v;
                         
@@ -107,7 +87,7 @@ let rec build liblist private_branch_anchor repo client set_meta get_meta rw = (
                          
                         )
 
-            | "read" -> print "read build" client;
+            | "read" -> 
                 ignore @@ getvalue private_branch_anchor lib client get_meta
             
             | _ -> failwith "wrong option for rw");
@@ -120,6 +100,7 @@ let rec build liblist private_branch_anchor repo client set_meta get_meta rw = (
 (*generating key of 2B *)
 let gen_write_key () = 
   let str = [|"1";"2";"3";"4";"5";"6";"7";"8";"9";"0";"a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m";"n";"o";"p";"q";"r";"s";"t";"u";"v";|] in
+  (* let key = (Array.get str (Random.int 2))^(Array.get str (Random.int 2)) in *)
   let key = (Array.get str (Random.int 32))^(Array.get str (Random.int 32)) in
   key
 
@@ -147,19 +128,14 @@ let create_or_get_public_branch repo ip =
   Scylla_kvStore.clone ~src:b_master ~dst:(ip ^ "_public")
 
 let publish branch1 branch2 publish_meta client = (*changes of branch2 will merge into branch1*)
-    (* Printf.printf "\npublish of %s" branch2; *)
-    print "publish" client;
     let stime = Unix.gettimeofday() in
         ignore @@ Scylla_kvStore.merge_with_branch ~info:(fun () -> Irmin.Info.empty) branch1 branch2;
     let etime = Unix.gettimeofday() in
     let diff = etime -. stime in
-    Printf.printf "  (%f)" diff;
     updateMeta publish_meta "publish_merge" diff
 
-let publish_to_public repo ip publish_meta = 
-    print "publish_to_public" ip;
-    (* Printf.printf "\npublishing..."; *)
-    create_or_get_public_branch repo ip >>= fun public_branch_anchor ->
+let publish_to_public repo ip replica publish_meta = 
+    create_or_get_public_branch repo replica >>= fun public_branch_anchor ->
     (*changes of 2nd arg branch will merge into first*)
     ignore @@ publish public_branch_anchor (ip ^ "_private") publish_meta ip;
     Lwt.return_unit 
@@ -175,136 +151,130 @@ let filter_public branchList =
   List.filter filter_str branchList
 
 
-let refresh repo client refresh_meta =
-  print "refresh" client;
-  (* Printf.printf "\nrefreshing..."; *)
+let refresh repo replica refresh_meta =
   (*merge current branch with the detached head of other*) 
-  create_or_get_public_branch repo client >>= fun public_branch_anchor ->
+  create_or_get_public_branch repo replica >>= fun public_branch_anchor ->
   Scylla_kvStore.Branch.list repo >>= fun branchList -> 
-  (* List.iter (fun x -> Printf.printf " *%s " x) branchList; *)
-  let branchList = filter_public branchList in
-  (* List.iter (fun x -> Printf.printf " ~%s " x) branchList; *)
-
-  (* Scylla_kvStore.Branch.get repo (client ^ "_public") >>= fun current_head_cmt ->
-                Scylla_kvStore.of_commit current_head_cmt >>= fun current_head -> *)
-                
-                mergeOpr_help branchList public_branch_anchor (client ^ "_public") repo refresh_meta  (*merge is returning unit*)
-  (* mergeOpr branchList current_head (client ^ "_public") repo refresh_meta; *)
-
-  (* mergeBranches current_head public_branch_anchor refresh_meta *)
   
-  (* create_or_get_private_branch repo client >>= fun private_branch_anchor ->
-  mergeBranches public_branch_anchor private_branch_anchor refresh_meta                                                                                                  *)
-
-
-(* let squash repo private_branch_str old_commit = 
-  Scylla_kvStore.Branch.get repo private_branch_str >>= fun other_head_cmt ->
-  let latest_tree = Scylla_kvStore.Commit.tree other_head_cmt in
-
-  (* Scylla_kvStore.Branch.get repo public_branch_str >>= fun old_commit -> *)
-  Scylla_kvStore.Commit.v repo ~info:(Irmin.Info.empty) ~parents:[Scylla_kvStore.Commit.hash old_commit] latest_tree >>= fun new_commit -> 
-  (* print "%s" (Irmin.Type.to_string (Scylla_kvStore.Commit.t repo) new_commit); *)
-
-  Scylla_kvStore.of_branch repo private_branch_str >>= fun private_branch_anchor ->
-  Scylla_kvStore.Head.set private_branch_anchor new_commit *)
-
-
-let post_operate_help opr_load private_branch_anchor repo client total_opr_load flag set_meta get_meta publish_meta refresh_meta old_commit =
-  (* Printf.printf "\nPost: client %s:" client; *)
-  print "post_operate_help" client;
+  let branchList = filter_public branchList in
+        
+  mergeOpr_help branchList public_branch_anchor (replica ^ "_public") repo refresh_meta  (*merge is returning unit*)
+  
+  exception File_not_found of string
+  
+let post_operate_help opr_load private_branch_anchor repo client replica total_opr_load flag set_meta get_meta publish_meta refresh_meta =
   let write_keylist = generate_write_key_list opr_load in (*generate_write_key_list is generating key for write operation*)
-  (* Printf.printf "\n";
-  List.iter (fun key -> Printf.printf "k=%s " key) write_keylist; *)
   ignore @@ build write_keylist private_branch_anchor repo client set_meta get_meta "post_write";
 
   ignore @@ build write_keylist private_branch_anchor repo client set_meta get_meta "read";
-  ignore @@ (create_or_get_public_branch repo client >>= fun public_branch_anchor ->
-(* Printf.printf " publishing... "; *)
-  ignore @@ publish_to_public repo client publish_meta;
+
+  (* ignore @@ (create_or_get_public_branch repo client >>= fun public_branch_anchor -> *)
+  (* Printf.printf "\npublishing... "; *)
+  (* ignore @@ publish_to_public repo client publish_meta *)
+  (* let oc =
+    try open_out_bin "/home/shashank/benchmark_church_infra/benchmark/counter/lockfile"
+    with Sys_error _ -> Printf.printf "file not found"; raise (File_not_found "file")
+  in
+  Unix.lockf (Unix.descr_of_out_channel oc) Unix.F_LOCK 0;
+  ignore @@ publish_to_public repo client replica publish_meta; 
+  Unix.lockf (Unix.descr_of_out_channel oc) Unix.F_ULOCK 0;
+  close_out oc; *)
   
-  ignore @@ build write_keylist public_branch_anchor repo client set_meta get_meta "read";
-
-(* Printf.printf " squashing... ";
-  ignore (old_commit >>= fun old_commit ->
-                  squash repo (client^"_public") old_commit); *)
-
-(* Printf.printf " refreshing... "; *)
-  ignore @@ refresh repo client refresh_meta;
   (* ignore @@ build write_keylist public_branch_anchor repo client set_meta get_meta "read"; *)
-  Lwt.return_unit)
+  (* Printf.printf "\nrefreshing... "; *)
+  ;ignore @@ refresh repo replica refresh_meta
+  
+  (* Lwt.return_unit) *)
   
 
-let rec operate opr_load private_branch_anchor repo client total_opr_load flag done_opr set_meta get_meta publish_meta refresh_meta loop_count old_commit =
-  print "operate" client;
+let rec operate opr_load private_branch_anchor repo client replica total_opr_load flag done_opr set_meta get_meta publish_meta refresh_meta =
   let rw_load = opr_load/2 in
   
-  (* Random.init ((Unix.getpid ()) + loop_count); *)
-  post_operate_help rw_load private_branch_anchor repo client total_opr_load flag set_meta get_meta publish_meta refresh_meta old_commit;
+  post_operate_help rw_load private_branch_anchor repo client replica total_opr_load flag set_meta get_meta publish_meta refresh_meta;
       
   (*this will make the keys generated in 2^x groups*)
-  let new_opr_load, flag = 
+  (* let new_opr_load, flag = 
   if (done_opr + (2 * opr_load)) < total_opr_load then
       ((2 * opr_load), true)
   else    
       ((total_opr_load - done_opr), false)
-  in
-  
-  let done_opr = done_opr + new_opr_load in
+  in *)
+  let new_opr_load, flag = 
+    if (done_opr + 100) < total_opr_load then
+        ((opr_load + 100), true)
+    else    
+        ((total_opr_load - done_opr), false)
+    in
 
-  if flag=true then (*flag denotes if it is a last round of operation or not. true = more rounds are there, false = no more rounds*)
-      let loop_count = loop_count + 1 in
-      operate new_opr_load private_branch_anchor repo client total_opr_load flag done_opr set_meta get_meta publish_meta refresh_meta loop_count old_commit
-  else  if new_opr_load != 0 then(
+  let done_opr = done_opr + new_opr_load in
+  (* Printf.printf "\n\nnew operation load: %d  done opr load = %d\n" new_opr_load done_opr; *)
+  if flag=true then( (*flag denotes if it is a last round of operation or not. true = more rounds are there, false = no more rounds*)
+  (* Printf.printf "\ncalling operate on recursion\n"; *)
+      operate new_opr_load private_branch_anchor repo client replica total_opr_load flag done_opr set_meta get_meta publish_meta refresh_meta)
+  else  if new_opr_load != 0 then( 
+    (* Printf.printf "\nrecursion on operate over\n"; *)
       let rw_load = new_opr_load/2 in
       
-      let loop_count = loop_count + 1 in
-      (* Random.init ((Unix.getpid ()) + loop_count); *)
-      post_operate_help rw_load private_branch_anchor repo client total_opr_load flag set_meta get_meta publish_meta refresh_meta old_commit
+      post_operate_help rw_load private_branch_anchor repo client replica total_opr_load flag set_meta get_meta publish_meta refresh_meta
   
-  )  
-      (* operate_help new_opr_load private_branch_anchor repo client total_opr_load flag set_meta get_meta *)
-
-
-let special_fun repo client = 
-  (* create_or_get_public_branch repo client >>= fun public_branch_anchor ->
+  )else ()
+  (* (Printf.printf "\nrecursion on operate over and load is 0\n";) *)
+    
   
-  ignore @@ Scylla_kvStore.set_exn ~info:(fun () -> Irmin.Info.empty) 
-  public_branch_anchor ["AA"] (Int64.of_int 11);
-
-  Scylla_kvStore.Branch.get repo (client^"_public") >>= fun old_commit -> *)
-  Lwt.return "old_commit"
-
-
-let buildLibrary ip client total_opr_load set_meta get_meta publish_meta refresh_meta =
-  print "buildlibrary" client;
+let buildLibrary ip client replica total_opr_load set_meta get_meta publish_meta refresh_meta =
   let conf = Irmin_scylla.config ip in
   Scylla_kvStore.Repo.v conf >>= fun repo ->
   
   create_or_get_private_branch repo client >>= fun private_branch_anchor ->
   
-  let old_commit = special_fun repo client in
-  (* ignore old_commit; *)
-  let opr_load = 2 in 
-  let done_opr = 2 in
-  operate opr_load private_branch_anchor repo client total_opr_load true done_opr set_meta get_meta publish_meta refresh_meta 0 old_commit;
+  let opr_load = 100 in 
+  let done_opr = 100 in
+  operate opr_load private_branch_anchor repo client replica total_opr_load true done_opr set_meta get_meta publish_meta refresh_meta;
+
   (* ignore @@ refresh repo client refresh_meta; *)
+  (* Printf.printf "\npublishing... "; *)
+  
+  let oc =
+    try open_out_bin "/home/shashank/benchmark_church_infra/benchmark/counter/lockfile"
+    with Sys_error _ -> Printf.printf "file not found"; raise (File_not_found "file")
+  in
+  Unix.lockf (Unix.descr_of_out_channel oc) Unix.F_LOCK 0;
+  ignore @@ publish_to_public repo client replica publish_meta; 
+  Unix.lockf (Unix.descr_of_out_channel oc) Unix.F_ULOCK 0;
+  close_out oc;
+
+
+  
+
   Lwt.return_unit 
+
+let singleread ip client set_meta get_meta = 
+  let conf = Irmin_scylla.config ip in
+  Scylla_kvStore.Repo.v conf >>= fun repo ->
+  
+  create_or_get_private_branch repo client >>= fun private_branch_anchor ->
+  (* ignore @@ build ["ee"] private_branch_anchor repo client set_meta get_meta "post_write"; *)
+  let stime = Unix.gettimeofday() in
+  ignore @@ build ["ee"] private_branch_anchor repo client set_meta get_meta "read";
+  let etime = Unix.gettimeofday() in
+    Printf.printf "read time: %f" (etime -. stime);
+    Lwt.return_unit
 
 let _ =
   let hostip = Sys.argv.(1) in
   let client = Sys.argv.(2) in
   let total_opr_load = Sys.argv.(3) in (* no. of keys to insert *)
-  print "beginning" client;
+  let replica = Sys.argv.(4) in 
   Random.init (Unix.getpid ());
 
   let set_meta = ref ("", 0.0, 0) in 
   let get_meta = ref ("", 0.0, 0) in 
-  (* let merge_meta = ref ("", 0.0, 0) in  *)
   let publish_meta = ref ("", 0.0, 0) in 
   let refresh_meta = ref ("", 0.0, 0) in 
 
   
-  ignore @@ buildLibrary hostip client (int_of_string total_opr_load) set_meta get_meta publish_meta refresh_meta;
+  ignore @@ buildLibrary hostip client replica (int_of_string total_opr_load) set_meta get_meta publish_meta refresh_meta;
+  (* ignore @@ singleread hostip client set_meta get_meta; *)
 
   let (set_msg, set_time, set_count) = !set_meta in 
   let (get_msg, get_time, get_count) = !get_meta in 
@@ -314,5 +284,5 @@ let _ =
   let total_time = set_time +. get_time +. publish_time +. refresh_time in 
   (* Printf.printf "\n\nset_time = %f;set_count = %d;get_time = %f;get_count = %d;publish_time = %f;publish_count = %d;refresh_time = %f;refresh_count = %d;total_time = %f" set_time set_count get_time get_count publish_time publish_count refresh_time refresh_count total_time; *)
 
-  Printf.printf "\n\n\n%f; %d; %f; %d; %f; %d; %f; %d; %f\n\n" set_time set_count get_time get_count publish_time publish_count refresh_time refresh_count total_time;
+  Printf.printf "\n%f; %d; %f; %d; %f; %d; %f; %d; %f" set_time set_count get_time get_count publish_time publish_count refresh_time refresh_count total_time;
   
